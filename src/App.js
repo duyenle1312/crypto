585c181d911aac6efa1f29c47f6e3675dcb97c6b
duyenlnmd <dle@codixis.com>
2021-11-29 23:52:43 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./Home";
 import Buy from "./Buy";
 
 function App() {
   return (
       <BrowserRouter>
-          <Switch>
-              <Route path='/' component={Home} exact={true} />
-              <Route path='/buy' component={Buy} exact={true} />
-              <Route component={Home} />
-          </Switch>
+          <Routes>
+              <Route path='/' element={<Home/>} exact={true} />
+              <Route path='/buy' element={<Buy />} exact={true} />
+              <Route element={<Home />} />
+          </Routes>
       </BrowserRouter>
   );
 }
